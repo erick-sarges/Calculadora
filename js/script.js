42bc6f28787dd5e22f9c9ab258ec2ed92946937b
erick <erick.sarges0223@gmail.com>
2022-11-12 15:13:03 -0300

    estrutura JS criadada

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
+const previuosOperationText = document.querySelector("#calc-previous-operation");
+const currentOperationText = document.querySelector("#calc-current-operation");
+const buttons = document.querySelectorAll("#buttons-conteiner button");
 
+class Calculator {
+    constructor(previuosOperationText, currentOperationText){
+        this.previuosOperationText = previuosOperationText
+        this.currentOperationText =currentOperationText
+        this.currentOperation = ""
+
+    };
+};
+
+const calc = new Calculator(previuosOperationText, currentOperationText);
+
+buttons.forEach((btn)=>{
+
+    btn.addEventListener('click', (e)=>{
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+            console.log(value)
+        }else{
+            console.log("Op:" + value)
+        }
+    });
+});
 
